d512c9b762bc4b199249cab8d8081702122b80cd
blackcatcoding <xypip@qq.com>
2023-02-05 10:15:54 +0800

    2.5

--- Python全栈开发/前端开发/第二章 JavaScript/02-node后端运行和js刷题/2/test02.js
+++ Python全栈开发/前端开发/第二章 JavaScript/02-node后端运行和js刷题/2/test02.js
@@ -1,29 +1,14 @@
 let buf = "";
 
-process.stdin.on("readable", function () {
+process.stdin.on("readable", function(){
     let chunk = process.stdin.read();
-    if (chunk) buf += chunk.toString();
+    if(chunk) buf += chunk.toString();
 });
 
-let path = [];
-let st = [];
-
-let dfs = (u, n) => {
-    if (u === n) console.log(path.join(' '));
-    else {
-        for (let i = 1; i <= n; i++) {
-            if (!st[i]) {
-                st[i] = true;
-                path.push(i);
-                dfs(u + 1, n);
-                path.pop();
-                st[i] = undefined;
-            }
-        }
-    }
-}
-
-process.stdin.on('end', function () {
-    let n = parseInt(buf);
-    dfs(0, n);
+process.stdin.on("end", function(){
+    let [a, b, c] = buf.split(" ").map(x => {return parseInt(x)});
+   console.log((a + b) * c); 
 });
+
+
+// P109
