22d00e4e3b88adabd60155254bc4bc18750441eb
blackcatcoding <xypip@qq.com>
2023-03-03 16:01:10 +0800

    2023.3.3 jquery14

---
+++ C++项目开发/MFC进阶和动态库注入辅助/06-MFC DC设备上下文绘图/Demo/Demo/DemoDlg.cpp
@@ -0,0 +1,226 @@
+﻿
+// DemoDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Demo.h"
+#include "DemoDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CDemoDlg 对话框
+
+
+
+CDemoDlg::CDemoDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DEMO_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CDemoDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CDemoDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON1, &CDemoDlg::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &CDemoDlg::OnBnClickedButton2)
+	ON_BN_CLICKED(IDC_BUTTON3, &CDemoDlg::OnBnClickedButton3)
+	ON_WM_NCPAINT()
+END_MESSAGE_MAP()
+
+
+// CDemoDlg 消息处理程序
+
+BOOL CDemoDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CDemoDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CDemoDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CPaintDC dc(this);
+		dc.TextOutW(100, 300, _T("这部分不会被无效"));
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CDemoDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CDemoDlg::OnBnClickedButton1()
+{
+	CDC* pDC = GetDC();
+
+	pDC->MoveTo(CPoint(0, 0));
+	pDC->LineTo(CPoint(100, 200));
+
+	ReleaseDC(pDC);
+}
+
+
+void CDemoDlg::OnBnClickedButton2()
+{
+	CDC* pDC = GetDC();
+
+	CPen pen;
+	pen.CreatePen(PS_SOLID, 2, RGB(0, 255, 0));
+	CPen* pOldPen = pDC->SelectObject(&pen);
+
+	pDC->Rectangle(10, 10, 100, 200);
+
+	CBrush bsh(RGB(255, 200, 0));
+	CBrush* pOldBsh = pDC->SelectObject(&bsh);
+
+	pDC->FillRect(CRect(300, 400, 500, 500), &bsh);
+
+	pDC->Ellipse(200, 200, 300, 400);
+
+	pDC->SelectObject(pOldBsh);
+	pDC->SelectObject(pOldPen);
+
+	pDC->RoundRect(200, 200, 300, 500, 50, 50);
+
+	pDC->RoundRect(500, 200, 600, 300, 100, 100);
+
+	pDC->FillRect(CRect(600, 400, 700, 500), pOldBsh);
+
+	ReleaseDC(pDC);
+}
+
+
+void CDemoDlg::OnBnClickedButton3()
+{
+	CDC* pDC = GetDC();
+
+	pDC->SetBkColor(RGB(0, 255, 255));
+	pDC->SetBkMode(TRANSPARENT);
+
+	pDC->TextOutW(400, 400, L"黑猫编程：noi.hioier.com");
+	pDC->DrawText(L"在线刷题：www.hioier.com", CRect(300, 300, 600, 500), DT_CENTER);
+
+	ReleaseDC(pDC);
+}
+
+
+void CDemoDlg::OnNcPaint()
+{
+	CWindowDC dc(NULL);
+	// CWindowDC dc(NULL);//为NULL时表示桌面窗口
+	dc.TextOut(0, 0, _T("我绘制的标题"));
+	// TODO: 在此处添加消息处理程序代码
+	// 不为绘图消息调用 CDialogEx::OnNcPaint()
+}
---
+++ C++项目开发/MFC进阶和动态库注入辅助/Demo/Demo/resource.h
@@ -0,0 +1,27 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Demo.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_DEMO_DIALOG                 102
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP_FJ                   130
+#define IDB_BITMAP_FJ2                  131
+#define IDB_BITMAP1                     132
+#define IDB_BITMAP_LOGO                 132
+#define IDC_BTN_LoadNewImg              1000
+#define IDC_STATIC_PIC                  1001
+#define IDC_BTN_LOAD_LOGO               1002
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
