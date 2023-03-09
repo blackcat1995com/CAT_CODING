7f4abbf087a4cac7692495c00cc32a3a67a09217
blackcatcoding <xypip@qq.com>
2023-03-09 16:46:51 +0800

    23.3.9

---
+++ C++项目开发/MFC进阶和动态库注入辅助/Demo/Demo/CDlgPage1.cpp
@@ -0,0 +1,34 @@
+﻿// CDlgPage1.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "Demo.h"
+#include "afxdialogex.h"
+#include "CDlgPage1.h"
+
+
+// CDlgPage1 对话框
+
+IMPLEMENT_DYNAMIC(CDlgPage1, CDialogEx)
+
+CDlgPage1::CDlgPage1(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DLG_PAGE1, pParent)
+{
+
+}
+
+CDlgPage1::~CDlgPage1()
+{
+}
+
+void CDlgPage1::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CDlgPage1, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CDlgPage1 消息处理程序
