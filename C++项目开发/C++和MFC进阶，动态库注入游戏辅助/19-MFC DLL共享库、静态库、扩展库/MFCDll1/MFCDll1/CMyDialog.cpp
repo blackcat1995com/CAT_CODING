7d68310861804c4456828289e780eb19b26b2067
blackcatcoding <xypip@qq.com>
2023-03-29 17:48:53 +0800

    2023.3.29 MFC dll

---
+++ C++项目开发/C++和MFC进阶，动态库注入游戏辅助/19-MFC DLL共享库、静态库、扩展库/MFCDll1/MFCDll1/CMyDialog.cpp
@@ -0,0 +1,38 @@
+﻿// CMyDialog.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "MFCDll1.h"
+#include "afxdialogex.h"
+#include "CMyDialog.h"
+
+
+// CMyDialog 对话框
+
+IMPLEMENT_DYNAMIC(CMyDialog, CDialogEx)
+
+CMyDialog::CMyDialog(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG, pParent)
+{
+
+}
+
+CMyDialog::~CMyDialog()
+{
+}
+
+void CMyDialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CMyDialog, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMyDialog 消息处理程序
+__declspec(dllexport) void ShowDialog() {
+	CMyDialog dlg;
+	dlg.DoModal();
+}
---
+++ C++项目开发/C++和MFC进阶，动态库注入游戏辅助/19-MFC DLL共享库、静态库、扩展库/MFCDll2/MFCDll2/MFCDll2.cpp
@@ -0,0 +1,13 @@
+﻿// MFCDll2.cpp: 定义 DLL 的初始化例程。
+//
+
+#include "pch.h"
+#include "framework.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+extern "C" CString GetString() {
+	return CString("noi.hioier.com");
+}
